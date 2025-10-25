e663a7056b5a67c7b80c582868bf05236283c54f
UncleKostyaa <unclekostya7670@outlook.com>
2025-10-25 17:28:47 +0300

    some UI upgrades

--- app/src/main/java/com/unclekostya/motivationapp/MainActivity.kt
+++ app/src/main/java/com/unclekostya/motivationapp/MainActivity.kt
@@ -7,17 +7,35 @@ import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -42,12 +60,79 @@ class MainActivity : ComponentActivity() {
     }
 
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             MotivationAppTheme {
-                QuoteApp(viewModel=viewModel)
+                Scaffold(
+                    topBar = {
+                        CenterAlignedTopAppBar(
+                            colors = TopAppBarDefaults.topAppBarColors(
+                                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                                titleContentColor = MaterialTheme.colorScheme.primary
+                            ),
+                            title = {
+                                Text("Motivation app", fontWeight = FontWeight.Bold)
+                            }
+                        )
+                    },
+                    bottomBar = {
+                        BottomAppBar(
+                            actions = {
+                                IconButton(
+                                    onClick = {},
+                                    modifier = Modifier
+                                        .padding(start = 60.dp)
+                                ) {
+                                    Icon(
+                                        painter = painterResource(R.drawable.home_24dp_e3e3e3_fill0_wght400_grad0_opsz24),
+                                        contentDescription = "home",
+                                        modifier = Modifier
+                                            .size(48.dp)
+
+                                    )
+                                }
+                                IconButton(
+                                    onClick = {},
+                                    modifier = Modifier
+                                        .padding(start = 60.dp)
+                                ) {
+                                    Icon(
+                                        painter = painterResource(R.drawable.star_24dp_e3e3e3_fill0_wght400_grad0_opsz24),
+                                        contentDescription = "favourites",
+                                        modifier = Modifier
+                                            .size(48.dp)
+
+                                    )
+                                }
+                                IconButton(
+                                    onClick = {},
+                                    modifier = Modifier
+                                        .padding(start = 60.dp)
+                                ) {
+                                    Icon(
+                                        painter = painterResource(R.drawable.settings_24dp_e3e3e3_fill0_wght400_grad0_opsz24),
+                                        contentDescription = "settings",
+                                        modifier = Modifier
+                                            .size(48.dp)
+
+                                    )
+                                }
+                            }
+
+                        )
+                    }
+
+                ) { innerPadding ->
+                    Surface(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                    ) {
+                        QuoteApp(viewModel=viewModel)
+                    }
+                }
             }
         }
     }
@@ -95,10 +180,49 @@ fun QuoteApp(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        quote?.let {
-            Text(text = it.quoteText)
-            Text(text = it.authorName)
-        } ?: Text("Загрузка цитаты...")
+        Card(
+           colors = CardDefaults.cardColors(
+               containerColor = MaterialTheme.colorScheme.surfaceVariant,
+           ),
+            modifier = modifier
+                .size(height = 230.dp, width = 360.dp)
+        ) {
+            quote?.let {
+                Text(
+                    text = it.quoteText,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 24.sp,
+                    modifier = modifier
+                        .padding(14.dp)
+                )
+                Text(
+                    text = it.authorName,
+                    textAlign = TextAlign.Left,
+                    fontSize = 18.sp,
+                    fontStyle = FontStyle.Italic,
+                    modifier = modifier
+                    .padding(16.dp))
+            } ?: Text("Loading...")
+        }
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = modifier
+                .padding(top = 12.dp)
+        ) {
+            Button(onClick = {
+                viewModel.loadQuote()
+            }) {
+                Text("Next quote")
+            }
+            Button(
+                onClick = {
 
+                },
+                modifier = modifier
+                    .padding(start = 10.dp)
+            ) {
+                Text("To favourites!")
+            }
+        }
     }
 }
